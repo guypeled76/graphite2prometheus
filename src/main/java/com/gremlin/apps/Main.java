13f7b2d3f9df68a73211fb2a023e73887ac79752
guypeled76 <guypeled76@gmail.com>
2018-09-29 09:24:09 +0300

    Add support for external console

--- src/main/java/com/gremlin/apps/Main.java
+++ src/main/java/com/gremlin/apps/Main.java
@@ -1,7 +1,7 @@
 package com.gremlin.apps;
 
+import java.io.Console;
 import java.io.File;
-import java.io.FileReader;
 import java.util.Scanner;
 
 import com.gremlin.engine.Expression;
@@ -13,20 +13,14 @@ import com.gremlin.engine.Processor;
 class Main {
    public static void main(String[] args)  throws Exception  {
       
-        // Check if there is a file argument
-        if(args.length == 0) {
-            System.out.println("Please provide a file argument.");
-            return;
-        }
+        // If there is a valid extenrnal console
+        if(System.console() != null) {
+                
+            // Start interacting with the user
+            System.out.println("Enter expressions:");
 
-        // Get file
-        File file =  new File(args[0]); 
-
-        // If there is a valid file
-        if(file.exists() && !file.isDirectory()) { 
-
-            // Create the file scanner
-            Scanner scanner = new Scanner(file);
+            // Create the input scanner
+            Scanner scanner = new Scanner(System.in);
 
             // The arithmetic processor
             Processor processor = new Processor();
@@ -34,24 +28,44 @@ class Main {
             // The current line from the console
             String line = null;
 
+            boolean exit = false;
+
             // Read next line and process or exit
-            while((line = readLine(scanner)) != null)
+            while(!exit && ((line = readLine(scanner)) != null))
             {
-                // Evaluate line
-                Expression result = processor.execute(line);
-                if(!Expression.isEmpty(result))
-                {
-                    // Print the value of the evaluated expression
-                    System.out.println(result);
+                // TODO: move function parsing to processor
+                switch(line) {
+                    case "exit()":
+                        exit = true;
+                        break;
+                    case "debug()":
+                        // Write the processor state
+                        System.out.println(processor.debug());
+                        break;
+                    default:
+                        // Evaluate line
+                        Expression result = processor.execute(line);
+                        if(!Expression.isEmpty(result))
+                        {
+                            // Print the value of the evaluated expression
+                            System.out.println(result);
+                        }
+                        break;
                 }
+                
+
+
             } 
 
             // Write the processor state
             System.out.println(processor.debug());
+
+        } else {
+            System.out.println("Please provide an extenrnal console that supports aninput stream.");
+            return;
         }
-        else {
-            System.out.println("Please provide a valid file.");
-        }
+
+
 
    }
 
@@ -61,16 +75,27 @@ class Main {
      */
     private static String readLine(Scanner scanner) {
         
+        // Print the input sign
+        System.out.print(">> ");
+
         if(!scanner.hasNext()) {
             return null;
         }
 
+        
+
+        // Wait for next line
         String line = scanner.nextLine();
         if(line != null) {
             // normalize line
             line = line.trim();
         }
+
+        // If is an empty string
+        if(line != "") {
+            return line;
+        }
         
-        return line;
+        return null;
     }
 }
